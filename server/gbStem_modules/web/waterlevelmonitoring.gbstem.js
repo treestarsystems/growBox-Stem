5770d1545a3a743243117aef4b7d4508c6f153fc
Michael Bogle <support@mjns.it>
2019-04-22 05:38:15 -0400

    add bottom sensor logic

--- server/gbStem_modules/web/waterlevelmonitoring.gbstem.js
+++ server/gbStem_modules/web/waterlevelmonitoring.gbstem.js
@@ -1,15 +1,24 @@
+/*
+Water Level Monitoring:
+Purpose - Read the status of the highest and lowest sensor in the bucket/
+	  reservoir.
+Inputs  - Float valves connected to compatible GPIO pins and a Motorized/
+	  Solenoid valve (2 Wire)that is controlled via a relay
+Action  - When topFloat (status 1) OR bottomFloat (status 0) is falling
+	  /low/hanging the valve is opened/stays opened. When topFloat is
+	  high/up (status 0) the valve should be closed.
+*/
 const Gpio = require('onoff').Gpio;
 //Define our GPIO pins to wach and control
 //Will replace hard-coded values later.
 const topFloat = new Gpio(17, 'out');
+const bottomFloat = new Gpio(18, 'out');
 //This opens the valve when called. Most likely I am not using this right.
 const intakeValve = new Gpio(5, 'out');
 
 //If relay is ON/Closed then turn it OFF/Open.
 function closeValve() {
-	console.log('Checking is Valve is Open.');
 	if (intakeValve.readSync() == 0) {
-		console.log('Now Closing');
 		intakeValve.writeSync(1);
 	}
 }
@@ -24,8 +33,9 @@ function monitorWaterLevel() {
 	//Water High, Close Valve.
 	if (topFloat.readSync() == 0) {
 		intakeValve.writeSync(1);
+	}
 	//Water Low, Open Valve.
-	} else {
+	if (bottomFloat.readSync() == 0 || topFloat.readSync() == 1) {
 		intakeValve.writeSync(0);
 	}
 }
@@ -41,4 +51,5 @@ setInterval(monitorWaterLevel, 500);
 process.on('SIGINT', () => {
   intakeValve.unexport();
   topFloat.unexport();
+  bottomFloat.unexport();
 });
