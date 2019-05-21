a797f51ba71680a51e061f70d04c1790d5ec866c
Michael Bogle <support@mjns.it>
2019-05-21 07:09:26 -0400

    working with setup script

--- setup/setup.gbstem.js
+++ setup/setup.gbstem.js
@@ -18,8 +18,6 @@ var argv = require('minimist')(process.argv.slice(2));
 
 /*
 What input do I need?
-hostname
-root server IP
 mount points beside / to monitor
 relay count - assign to variable then ask next ? that many times
 relay pin
@@ -28,12 +26,68 @@ temp sesnse id - parse dir for ID strings that start with 28-????
 console data refresh greater than 5secs
 */
 
-var questions = [{
-  type: 'input',
+var questions = [
+{
+  type: 'list',
   name: 'sysType',
   message: "What type of system is this?",
-}]
+  choices: ['Root', 'Stem', 'Branch', 'Flower'],
+  filter: function(val) {
+      return val.toLowerCase();
+  }
+},
+{
+  type: 'input',
+  name: 'hostname',
+  message: `Desired local hostname? (stem-${core.genRegular(5)})`,
+},
+{
+  type: 'input',
+  name: 'gbRootAddress',
+  message: `growBox-Root IP Address?`,
+},
+{
+  type: 'number',
+  name: 'relayCount',
+  message: `How many relays will be controlled?`,
+},
+{
+  type: 'list',
+  name: 'temperatureScale',
+  message: `Desired temperature scale?`,
+  choices: ['C - Celsius', 'F - Fahrenheit', 'K - Kelvin'],
+  filter: function(val) {
+      return val.toLowerCase();
+  }
+},
+{
+  type: 'input',
+  name: 'mountPoints',
+  message: `Other mount points?`,
+},
+{
+  type: 'input',
+  name: 'hostname',
+  message: `Desired hostname? (stem-${core.genRegular(5)})`,
+},
+{
+  type: 'input',
+  name: 'hostname',
+  message: `Desired hostname? (stem-${core.genRegular(5)})`,
+},
+{
+  type: 'input',
+  name: 'hostname',
+  message: `Desired hostname? (stem-${core.genRegular(5)})`,
+}
+
+
+]
 
 inquirer.prompt(questions).then(answers => {
-  console.log(`This is a ${answers['sysType']}`)
+  console.log(`System Type: ${answers['sysType']}`)
+  console.log(`Hostname: ${answers['hostname']}`)
+  console.log(`growBox-Root Address: ${answers['gbRootAddress']}`)
+  console.log(`Relay Count: ${answers['relayCount']}`)
+  console.log(`Temperature Scale: ${answers['temperatureScale']}`)
 })
